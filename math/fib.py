5f67f2f70e1425be9e2f4c1c4434328af843bb77
MrMagnusneo <mr.magnusneo@gmail.com>
2026-09-18 17:17:50 +0300

    Translate input prompts and output messages in Fibonacci script to English

--- math/fib.py
+++ math/fib.py
@@ -1,9 +1,9 @@
 
-fib = int(input("Введите порядковый номер числа фиббоначи: "))
+fib = int(input("Enter the index number of the Fibonacci number: "))
 
 list = [0, 1]
 
 for i in range (2, fib):
     list.append(list[0] + list[1])
     list.pop(0)
-print(f"Ответ: {list[1]}")
+print(f"Answer: {list[1]}")
